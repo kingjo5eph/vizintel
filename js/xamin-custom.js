66606dca9ac7d09fc7a1acbfc675e52a8e14e61e
kingjo5eph <126416188+kingjo5eph@users.noreply.github.com>
2024-09-21 10:56:42 +0600

    overwrite js

--- js/xamin-custom.js
+++ js/xamin-custom.js
@@ -378,16 +378,16 @@ $(document).ready(function() {
 
 
     // Disable right-click context menu
-  //   document.addEventListener('contextmenu', function (e) {
-  //       e.preventDefault();
-  //   });
+    document.addEventListener('contextmenu', function (e) {
+        e.preventDefault();
+    });
 
 
-  //   document.addEventListener('keydown', function (event) {
-  //     if (event.key === 'i' || event.key === 'I' || event.key === 'j' || event.key === 'J' || event.key === 'u' || event.key === 'U') {
-  //         event.preventDefault();
-  //     }
-  // });
+    document.addEventListener('keydown', function (event) {
+      if (event.key === 'i' || event.key === 'I' || event.key === 'j' || event.key === 'J' || event.key === 'u' || event.key === 'U') {
+          event.preventDefault();
+      }
+  });
   
 
 // Change h2 element when click tabs
